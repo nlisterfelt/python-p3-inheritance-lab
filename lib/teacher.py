461bd8be82ccb9d777e25c8c179c6d4d05c0a1e2
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-09-01 13:50:38 -0500

    super()

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,8 @@ from user import User
 import random
 
 class Teacher(User):
-
+    def __init__(self, first_name, last_name, knowledge=[None]):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
     def teach(self):
         pass
